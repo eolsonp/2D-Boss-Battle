ae1b10dac014745964137bd28b090ba90ba22568
Eric Olson <Eric.Olson3@iam.local>
2018-09-26 15:20:17 -0500

    WIP movement

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
 
+    [SerializeField]
+    private int lives = 3;
+
+    [SerializeField]
+    private string name = "Mario";
+
+    [SerializeField]
+    private float jumpHeight = 5, speed = 5;
+
+    
+    private bool isOnGround;
+    
+    private float horizontalInput;
+
+
 	// Use this for initialization
 	void Start () {
         Debug.Log("Hello");
@@ -11,6 +26,18 @@ public class PlayerCharacter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        GetInput();
+        Move();
 		
 	}
+    private void GetInput()
+    {
+       horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void Move()
+    {
+        rigidbody2dInstance.velocity = new Vector2(horizontalInput, 0);
+    }
 }
